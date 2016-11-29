b7a9eea587b8dd983aefaa0513ff57b768656c8f
yourUsername <your_email@example.com>
2016-11-28 23:40:31 -0500

    i broke it

--- walker.js
+++ walker.js
@@ -1,10 +1,49 @@
 // Get the walker image:
 var walker = document.getElementById('walker');
-
 // Configure motion params:
+var walkingLeft = true;
 var leftBorder = 0;
 var rightBorder = window.innerWidth - walker.offsetWidth;
+var speed = 20;
+var xPos = rightBorder;
+function update() {
+  if(walkingLeft){
+    xPos -= speed;
+  }
+  else {
+    xPos += speed;
+  }
+  // reposition the walker
+  walker.style.left = xPos + "px";
 
-// Have the stick figure start at the right border and start walking left
-// When he crosses the left border, have him turn around and start walking right (and vice versa)
-// Bonus: Make him turn around when you click on him
+  // Turn around when you've crossed a border:
+  if (xPos < leftBorder){
+    walker.classList.toggle("flip");
+    walkingLeft = !walkingLeft;
+  }
+  else if (xPos > rightBorder) {
+    walker.classList.toggle("!flip");
+    walkingLeft = walkingLeft;
+  }
+
+}
+walker.addEventListener('click', function(){
+  walker.classList.toggle("flip"),
+  walkingLeft = !walkingLeft;
+});
+//update interval
+setInterval(update, 100);
+//
+// walker.addEventListener('click', function(){
+// var url = 'http://quotesondesign.com/api/3.0/api-3.0.json';
+// $.ajax ({
+// 	url: url,
+// 	type: "GET",
+// 	dataType: "json"
+// }).done(function(){
+// 	console.log(json.quote);
+//   alert(json.quote);
+// }).fail(function(response){
+// console.log("It failed");
+// });
+// });
